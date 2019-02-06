6587f6e320ddab9f31538db9f7a2cdde1743264f
Ezra Free <ezrafree@gmail.com>
2019-02-05 20:56:43 -0700

    update the outlook online url and parameters

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure event details
-$start_date = "2016-03-12 10:00:00"; // 10am
-$end_date = "2016-03-12 16:00:00"; // 4pm
+$start_date = "2019-02-21 10:00:00"; // 10am
+$end_date = "2019-02-21 16:00:00"; // 4pm
 $event_name = "Example Event";
 $event_details = htmlspecialchars('Event Details Here\n\n--\nhttp://example.com');
 $event_location = "123 Main St, Example, NY 20001";
@@ -52,7 +52,7 @@ $event_duration = $hours . $minutes;
 						<li><a href="download.php?format=icalendar&amp;date=<?php echo str_replace('Z', '', $current_date); ?>&amp;text=<?= rawurlencode($event_name) ?>&amp;dates=<?= $fstart_date ?>/<?= $fend_date ?>&amp;details=<?= rawurlencode($event_details) ?>&amp;location=<?= rawurlencode($event_location) ?>">iCalendar</a></li>
 						<li><a href="https://www.google.com/calendar/event?action=TEMPLATE&amp;text=<?= rawurlencode($event_name) ?>&amp;dates=<?= $fstart_date ?>/<?= $fend_date ?>&amp;details=<?= str_replace("%5Cn", "%0A", rawurlencode($event_details)) ?>&amp;location=<?= rawurlencode($event_location) ?>" target="_blank">Google Calendar</a></li>
 						<li><a href="download.php?format=outlook&amp;date=<?= $current_date ?>&amp;text=<?= rawurlencode($event_name) ?>&amp;dates=<?= $fstart_date ?>Z/<?= $fend_date ?>Z&amp;details=<?= rawurlencode($event_details) ?>&amp;location=<?= rawurlencode($event_location) ?>">Outlook</a></li>
-						<li><a href="https://bay04.calendar.live.com/calendar/calendar.aspx?rru=addevent&amp;summary=<?= rawurlencode($event_name) ?>&amp;location=<?= rawurlencode($event_location) ?>&amp;description=<?= str_replace("%5Cn", "%0A", rawurlencode($event_details)) ?>&amp;dtstart=<?= rawurlencode($fstart_date) ?>&amp;dtend=<?= rawurlencode($fend_date) ?>" target="_blank">Outlook Online</a></li>
+						<li><a href="https://outlook.live.com/owa/?path=/calendar/view/month&rru=addevent&amp;startdt=<?= rawurlencode($fstart_date) ?>&amp;enddt=<?= rawurlencode($fend_date) ?>&amp;subject=<?= rawurlencode($event_name) ?>&amp;location=<?= rawurlencode($event_location) ?>&amp;body=<?= str_replace("%5Cn", "%0A", rawurlencode($event_details)) ?>" target="_blank">Outlook Online</a></li>
 						<li><a href="https://calendar.yahoo.com/?v=60&amp;view=d&amp;type=20&amp;title=<?= rawurlencode($event_name) ?>&amp;st=<?= rawurlencode($fstart_date) ?>&amp;dur=<?= rawurlencode($event_duration) ?>&amp;in_loc=<?= rawurlencode($event_location) ?>&amp;desc=<?= str_replace("%5Cn", "%0A", rawurlencode($event_details)) ?>&amp;url=<?= rawurlencode($return_url) ?>" target="_blank">Yahoo! Calendar</a></li>
 					</ul>
 				</div>
